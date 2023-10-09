04013abd6d915c6b97500c1aa62346aadd536eb8
AnnaYedoshina <yedoshina.java@gmail.com>
2023-10-09 12:14:29 +0300

    Tasks added

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-        int[] array = {1, 3, 6, 9, 12, 15, 18, 21, 24};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
         System.out.println("Элементы массива, кратные 3:");
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (array[i] % 3 == 0) {
                 System.out.println(array[i]);
             }
         }
     }
-
 }
+
